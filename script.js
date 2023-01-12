37570a58ceba000085ee7627fef8e1ca476bfab7
Fernando Bustillos <bustillos007@gmail.com>
2023-01-11 21:23:40 -0500

    fetch API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("testing");
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  //Pick a ramdon quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch Error here
+  }
+}
+
+//On Load
+getQuotes();
