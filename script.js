cc27344f703cf7ead3e3472d8d2cd237843f8b62
Fernando Bustillos <bustillos007@gmail.com>
2023-01-15 20:05:15 -0500

    added loader

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 // Show New Quote
 function newQuote() {
+  loading();
   //Pick a ramdon quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if author field is blank and replace with 'Unknown'
@@ -22,10 +35,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+
+  // Set Quote, Hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
